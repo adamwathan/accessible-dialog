73a7725099b2416b707792b97caa4c841ef6b1fc
Satya Kresna <satyakresna6295@gmail.com>
2020-05-14 21:26:42 +0800

    feat: accessible dialog

--- src/js/app.js
+++ src/js/app.js
@@ -1,27 +1,76 @@
-import page from "page";
-import Home from "./pages/home.js";
-import About from "./pages/about.js";
-import Users from "./pages/users.js";
-import NotFound from "./pages/notfound.js";
-import openMenu from "./behaviours/openMenu.js";
-import closeMenu from "./behaviours/closeMenu.js";
+import "wicg-inert";
+import trapFocus from "./behaviours/trapFocus.js";
 
-page('/', Home);
-page('/about', About);
-page('/users', Users);
-page('/users/:username', Users);
-page('*', NotFound);
-// Call it!
-page();
+const KEYCODE = {
+    ESC: 27
+};
+const dialog = document.querySelector('.dialog');
+const dialogWindow = dialog.querySelector('.dialog__window');
+const dialogMask = dialog.querySelector('.dialog__mask');
+let previousActiveElement;
 
-document.querySelector('button.header__hamburger-btn').addEventListener('click', function () {
-  openMenu();
+const $showDialogBtn = document.querySelectorAll('.show__dialog__btn');
+$showDialogBtn.forEach(btn => {
+    btn.addEventListener('click', openDialog);
 });
 
-document.querySelector('button.nav__hide-btn').addEventListener('click', function () {
-  closeMenu();
-});
+trapFocus(document.querySelector('.dialog__window'));
 
-document.querySelector('div.menu-underlay').addEventListener('click', function () {
-  closeMenu();
-});
+function openDialog() {
+    // Grab a reference to the previous activeElement.
+    // We'll want to restore this when we close the dialog.
+    previousActiveElement = document.activeElement;
+
+    // Quick and dirty way to make all of the siblings of our
+    // dialog inert.
+    Array.from(document.body.children).forEach(child => {
+        if (child !== dialog) {
+            child.inert = true;
+        }
+    });
+
+    // Make the dialog visible.
+    dialog.classList.add('opened');
+    // Hide scrolling body element
+    document.body.style.overflow = "hidden";
+
+    // Listen for things that should close the dialog
+    dialogMask.addEventListener('click', closeDialog);
+    dialogWindow.querySelectorAll('button').forEach(btn => {
+        btn.addEventListener('click', closeDialog);
+    });
+    document.addEventListener('keydown', checkCloseDialog);
+
+    // Finally, move focus into the dialog.
+    dialog.querySelector('button').focus();
+}
+
+function checkCloseDialog(e) {
+    if (e.keyCode === KEYCODE.ESC || e.key === 'Escape') {
+        closeDialog();
+    }
+}
+
+function closeDialog() {
+    // Clean up any event listeners.
+    dialogMask.removeEventListener('click', closeDialog);
+    dialogWindow.querySelectorAll('button').forEach(btn => {
+        btn.removeEventListener('click', closeDialog);
+    });
+    document.removeEventListener('keydown', checkCloseDialog);
+
+    // Uninert our siblings
+    Array.from(document.body.children).forEach(child => {
+        if (child !== dialog) {
+            child.inert = false;
+        }
+    });
+
+    // Remove Hide the dialog.
+    dialog.classList.remove('opened');
+    // Remove Hide scrolling body element
+    document.body.style.overflow = "visible";
+
+    // Restore focus to the previous active element.
+    previousActiveElement.focus();
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
     maxWidth: {
       '500px': '500px',
     },
+    fontFamily: {
+      'mono': ['Roboto Mono', 'monospace']
+    },
     extend: {}
   },
   variants: {},
